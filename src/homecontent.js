15dc58619db4fee94c0c90958b497a2b2497218a
Rohith Kumar B <rohithkumarcbaalraj@gmail.com>
2024-08-30 22:51:17 +0530

    Completed the page and added all contents

--- src/homecontent.js
+++ src/homecontent.js
@@ -5,10 +5,19 @@ function HomeContent() {
            <h1>
            Let’s Connect <br/>with Your Customers <br/>in Real Time
            </h1>
-           <p>
+           <p className='p'>
            Create styleguides to organize components,<br/>colors, text styles, and spacing tokens
            </p>
            <img src="cellfile.avif" className="cellfile" />
+           <div className="register-button">
+            <div style={{display:"flex",backgroundColor:"white",borderRadius:"10px",height:"80px"}}>
+            <p className='register'>Register using your email address</p>
+            <p className='email' style={{marginLeft:"-240px",marginTop:"50px"}}>company@company.com</p>
+            <button style={{marginRight:"30px"}}>Register</button>
+            </div>
+           <img src='phone.jpeg'className='phone' />
+           </div>
+           <img src='mobile.jpeg' className='mobile'/>
         </div>
     );
 }
